6197a4339c0024597f0b8639142cd4e9569d046d
niiquaye70 <iquaye5770@gmail.com>
2025-09-17 23:14:00 -0400

    Delete src directory

--- src/utils/seed.py
+++
@@ -1,8 +0,0 @@
-import random, torch, numpy as np
-
-def set_seed(seed=42):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
